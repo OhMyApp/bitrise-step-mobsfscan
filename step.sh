75090bc4b421b100930ee4f7aad47100ad80ac02
Mickael Malolepsy <mickael@oh-my-app.com>
2026-01-03 15:56:51 +0100

    feat: add all options

--- step.sh
+++ step.sh
@@ -4,5 +4,35 @@ set -ex
 # Install mobsfscan tool
 pip3 install --upgrade mobsfscan
 
-# Run analysis and save report
-python3 -m mobsfscan --output ${report_file_path} .
+# Build command
+
+cmd=(python3 -m mobsfscan)
+
+if [[ "$report_file_path" != "" ]]; then
+  cmd+=(--output "$report_file_path")
+fi
+
+if [[ "$output_format" != "txt" ]]; then
+  cmd+=(--"$output_format")
+fi
+
+if [[ "$config_file" != "" ]]; then
+  cmd+=(--config "$config_file")
+fi
+
+cmd+=(--type "$project_type")
+
+cmd+=(--multiprocessing "$multiprocessing")
+
+if [[ "$fail_on_warning" == "yes" ]]; then
+  cmd+=(--exit-warning)
+fi
+
+if [[ "$no_fail" == "yes" ]]; then
+  cmd+=(--no-fail)
+fi
+
+cmd+=("$project_path")
+
+# Run analysis
+"${cmd[@]}"
