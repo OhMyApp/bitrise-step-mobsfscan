f3d41916c7495261c5abee35320f9d09b1221d50
Mickael Malolepsy <mickael@oh-my-app.com>
2026-01-06 00:13:21 +0100

    feat: add test

---
+++ tests/project-ios/Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by MICKAEL on 06/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
